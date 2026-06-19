ec8a776971c54020afb33f334c98ac6373ebe316
dorfellous <dorfellous5@gmail.com>
2026-06-19 13:21:25 +0300

    Add spotlight data

---
+++ src/data/spotlights.js
@@ -0,0 +1,17 @@
+const spotlights = [
+  {
+    id: 'orchid-key-light',
+    name: 'Orchid Key Light',
+    position: [3.161, 3.200, -31.800],
+    target: [3.161, -1.200, -33.485],
+    intensity: 4.5,
+    color: '#ffffff',
+    angle: 0.35,
+    penumbra: 0.55,
+    distance: 12,
+    decay: 2,
+    helper: false,
+  },
+]
+
+export default spotlights
